adb6c691c4f963a9d6f84b51a178ba044f126ddb
KaioGalvao <kaioninin@hotmail.com>
2023-04-12 23:36:17 -0300

    build: added routing and its dependencies, start of development of the first components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
+
 function App() {
 
   return (
-    <h1 className='text-5xl font-bold text-violet-500'>CTP Acolhe 2</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
   )
 }
 
