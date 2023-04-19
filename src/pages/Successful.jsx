24a4b6e51dbdc7fdf08441631c4db2156118f89e
Matheus Portes <mportesif@gmail.com>
2023-04-19 01:08:25 -0300

    fix: api backend connection and some validations

---
+++ src/pages/Successful.jsx
@@ -0,0 +1,25 @@
+import { Logo } from "../components/Logo";
+import { useState } from 'react';
+
+export function Sucessfull(){
+
+    const [subscription, setSubscription] = useState({
+        nome: '',
+        email: '',
+        prontuario: '',
+        senha: ''
+    });
+
+    return(
+        <div
+            className="w-screen h-screen flex items-center flex-col justify-center p-4"
+        >
+            <p
+                className="text-4xl"
+            >
+                Olá, você acessou com sucesso o CTP Acolhe. Seja Bem-vindo(a)!
+            </p>
+            <Logo/>
+        </div>
+    );
+}
