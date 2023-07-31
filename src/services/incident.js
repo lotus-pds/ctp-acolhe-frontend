90b83b205b7234cbdf49f831244e7468cc38f2b4
Matheus Portes <mportesif@gmail.com>
2023-07-30 21:16:37 -0300

    feat: incident services mapping

---
+++ src/services/incident.js
@@ -0,0 +1,20 @@
+import { getResource, putResource, patchResource, postResource } from './config.js';
+import { serializeObjectToParam } from './utils.js';
+
+/* RECURSOS AUTENTICADO */
+
+export const postIncident = body => postResource('/usuario/autenticado/incidente', body);
+
+export const getIncidents = filters => getResource('/usuario/autenticado/incidente' + serializeObjectToParam(filters));
+
+/* RECURSOS GERAIS */
+
+export const getIncidentTypes = () => getResource('/tipoIncidente');
+
+export const getIncident = id => getResource(`/incidente/${id}`);
+
+export const cancelIncident = id => patchResource(`/usuario/autenticado/incidente/${id}/cancelar`);
+
+export const processIncident = id => patchResource(`/incidente/${id}/processar`);
+
+export const finishIncident = id => patchResource(`/incidente/${id}/finalizar`);
