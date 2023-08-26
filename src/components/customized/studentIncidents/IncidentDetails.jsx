c83caad89eee1e25c8fa79df7c8dec126d2bf1f2
Jéssica Sobral <85137657+jessica-sobral@users.noreply.github.com>
2023-08-26 15:33:21 -0300

    feat: adjust layout for viewing student incidents

--- src/components/customized/studentIncidents/IncidentDetails.jsx
+++ src/components/customized/studentIncidents/IncidentDetails.jsx
@@ -1,36 +1,28 @@
+import { ChatBubbleLeftRightIcon, ClipboardDocumentListIcon, StarIcon } from "@heroicons/react/24/solid";
 import {
     Dialog,
-    DialogHeader,
     DialogBody,
     DialogFooter,
-    Typography,
-    IconButton
+    DialogHeader,
+    IconButton,
+    Typography
 } from "@material-tailwind/react";
-import { GnButton } from "../../common/button/GnButton";
 import { useTranslation } from "react-i18next";
 import { convertDateBars } from "../../../common/general";
+import { GnButton } from "../../common/button/GnButton";
 import { ChipIncidentStatus } from "../gnIncidents/ChipIncidentStatus";
 
-const getIncidentTypes = (incidentTypes = []) => {
-    let types = incidentTypes.map(t => t.tipo);
-    return types.join(", ");
-}
-
 const getQuestionsAndAnswers = (questions = []) => {
-
     return (
         <div>
-            {questions.map(q => {
-                return (
-                    <div>
-                        <p>PERGUNTA</p>
-                        <p>{q.descricao}</p>
-                        <p>RESPOSTAS</p>
-                        <p>{q?.respostas.map(r => r.descricao).join(", ")}</p>
-                        <br />
-                    </div>
-                );
-            })}
+            {questions.sort((a, b) => a.ordem - b.ordem)
+                .map(q => {
+                    return (
+                        <div className="flex gap-2 mt-3 mx-5 ml-10">
+                            &#x2022;<Typography variant="h6">{q.descricao} <Typography>{q?.respostas.map(r => r.descricao).join(", ")}</Typography></Typography>
+                        </div>
+                    );
+                })}
         </div>
     );
 }
@@ -45,11 +37,12 @@ export function IncidentDetails(props) {
             <Dialog
                 open={open}
                 handler={() => { }}
-                className="max-h-[90vh] overflow-auto"
+                size="xl"
+                className="min-h-[60vh] max-h-[95vh] overflow-auto"
             >
-                <DialogHeader className="flex justify-between">
-                    <Typography>
-                        {incident?.assunto}
+                <DialogHeader className="flex justify-between pl-7">
+                    <Typography variant="h2" color="blue-gray" className="font-mouse sm:text-4xl text-2xl">
+                        Detalhes do incidente
                     </Typography>
                     <IconButton
                         color="blue-gray"
@@ -71,25 +64,67 @@ export function IncidentDetails(props) {
                                 d="M6 18L18 6M6 6l12 12"
                             />
                         </svg>
-                    </IconButton></DialogHeader>
-                <DialogBody>
-                    Data: {convertDateBars(new Date(incident?.dataIncidente))} | Status: <ChipIncidentStatus status={incident?.status?.idStatus} /><br />
-                    Tipos: {getIncidentTypes(incident?.tipos)}<br /><br />
-                    {getQuestionsAndAnswers(incident?.perguntas)}
+                    </IconButton>
+                </DialogHeader>
+                <hr />
+                <DialogBody className="py-0 px-10">
+                    <div className="my-3 mx-2">
+                        <Typography variant="h4" color="blue-gray" className="flex gap-4 py-2 pr-2">
+                            <ClipboardDocumentListIcon className="sm:w-6 w-5 inline" />
+                            Informações do Incidente
+                        </Typography>
+
+                        <span className="grid grid-cols-6 gap-y-4 my-4 ml-10">
+                            <span className="col-span-4">
+                                <Typography variant="h6">Assunto</Typography>
+                                <Typography>{incident?.assunto}</Typography>
+                            </span>
+                            <span className="col-span-1">
+                                <Typography variant="h6">Data de criação</Typography>
+                                <Typography>{convertDateBars(new Date(incident?.dataIncidente))}</Typography>
+                            </span>
+
+                            <span className="col-span-1">
+                                <Typography variant="h6">Status</Typography>
+                                <ChipIncidentStatus status={incident?.status?.idStatus} />
+                            </span>
+
+                            <span className="col-span-6">
+                                <Typography variant="h6">Temas</Typography>
+                                <Typography className="-ml-2 text-ellipsis overflow-hidden">
+                                    {incident?.tipos?.map((t) => (
+                                        <><StarIcon className="h-6 w-6 inline mr-1 ml-2 mb-1"></StarIcon>
+                                            {t?.tipo}</>
+                                    ))}
+                                </Typography>
+                            </span>
+                        </span>
+                    </div>
+                    <hr />
+                    <div className="my-3 mx-2">
+                        <Typography variant="h4" color="blue-gray" className="flex gap-4 py-2 pr-2">
+                            <ChatBubbleLeftRightIcon className="sm:w-6 w-5 inline" />
+                            Triagem
+                        </Typography>
+
+                        {getQuestionsAndAnswers(incident?.perguntas)}
+                    </div>
                 </DialogBody>
-                {(incident?.status?.idStatus != "CAN" && incident?.status?.idStatus != "FIN")
-                    ? <DialogFooter>
-                        <GnButton
-                            color="RED"
-                            onClick={() => {
-                                setWarningOpen(true);
-                            }}
-                        >
-                            {t("cancel")}
-                        </GnButton>
-                    </DialogFooter>
-                    : <></>
-                }
+                <div className="flex justify-end mr-3 -mt-6 mb-3">
+                    {(incident?.status?.idStatus != "CAN" && incident?.status?.idStatus != "FIN")
+                        ? <DialogFooter>
+                            <GnButton
+                                color="RED"
+                                onClick={() => {
+                                    setWarningOpen(true);
+                                }}
+                            >
+                                {t("cancel")}
+                            </GnButton>
+                        </DialogFooter>
+                        : <></>
+                    }
+                </div>
             </Dialog >
         </>
     );
--- src/pages/AdmIncidentDetails.jsx
+++ src/pages/AdmIncidentDetails.jsx
@@ -10,20 +10,14 @@ import { GnButton } from "../components/common/button/GnButton";
 import { ChipIncidentStatus } from "../components/customized/gnIncidents/ChipIncidentStatus";
 import { completeIncident, getIncident, processIncident } from "../services/incident";
 
-const getIncidentTypes = (incidentTypes = []) => {
-    let types = incidentTypes.map(t => t.tipo);
-    return types.join(", ");
-}
-
 const getQuestionsAndAnswers = (questions = []) => {
     return (
         <div>
             {questions.sort((a, b) => a.ordem - b.ordem)
                 .map(q => {
                     return (
-                        <div className="flex gap-2 my-5 mx-5">
-                            <ChatBubbleLeftRightIcon className="h-6 w-6 inline ml-2 mb-1"></ChatBubbleLeftRightIcon><Typography variant="h6">{q.descricao}</Typography>
-                            <Typography>{q?.respostas.map(r => r.descricao).join(", ")}</Typography>
+                        <div className="flex gap-2 my-3 mx-5 ml-10">
+                            &#x2022;<Typography variant="h6">{q.descricao} <Typography>{q?.respostas.map(r => r.descricao).join(", ")}</Typography></Typography>
                         </div>
                     );
                 })}
@@ -73,13 +67,13 @@ export function AdmIncidentDetails(props) {
                         </Typography>
                     </div>
                     <hr />
-                    <div className="my-5 ml-14 mr-14">
+                    <div className="my-3 mx-14">
                         <Typography variant="h4" color="blue-gray" className="flex gap-4 py-2 pr-2">
                             <UserIcon className="sm:w-6 w-5 inline" />
                             Informações do Aluno(a)
                         </Typography>
 
-                        <span className="grid grid-cols-12 gap-y-5 my-8 ml-10">
+                        <span className="grid grid-cols-12 gap-y-5 my-4 ml-10">
                             <span className="col-span-4">
                                 <Typography variant="h6">Nome</Typography>
                                 <Typography>{incident?.usuarioCopia?.nome}</Typography>
@@ -117,13 +111,13 @@ export function AdmIncidentDetails(props) {
                         </span>
                     </div>
                     <hr />
-                    <div className="my-5 ml-14 mr-14">
+                    <div className="my-3 mx-14">
                         <Typography variant="h4" color="blue-gray" className="flex gap-4 py-2 pr-2">
                             <ClipboardDocumentListIcon className="sm:w-6 w-5 inline" />
                             Informações do Incidente
                         </Typography>
 
-                        <span className="grid grid-cols-12 gap-y-5 my-8 ml-10">
+                        <span className="grid grid-cols-12 gap-y-5 my-4 ml-10">
                             <span className="col-span-6">
                                 <Typography variant="h6">Assunto</Typography>
                                 <Typography>{incident?.assunto}</Typography>
@@ -151,7 +145,7 @@ export function AdmIncidentDetails(props) {
                         </span>
                     </div>
                     <hr />
-                    <div className="my-5 ml-14 mr-14">
+                    <div className="my-3 mx-14">
                         <Typography variant="h4" color="blue-gray" className="flex gap-4 py-2 pr-2">
                             <ChatBubbleLeftRightIcon className="sm:w-6 w-5 inline" />
                             Triagem
@@ -159,7 +153,7 @@ export function AdmIncidentDetails(props) {
 
                         {getQuestionsAndAnswers(incident?.perguntas)}
                     </div>
-                    <div className="flex justify-end my-6 mx-5">
+                    <div className="flex justify-end mb-6 mr-5">
                         {(incident?.status?.idStatus == "PEN")
                             ? <GnButton
                                 color="BLUE"
